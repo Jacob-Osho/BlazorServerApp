b66b9cfa6350ce0a7c8d953bece936c4fa327d57
Jacob <akella2609@gmail.com>
2022-04-23 18:53:26 +0200

    realized popUp Delete Confirmation

---
+++ GameIndustry/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GameIndustry.Pages
+
+{
+    partial class Counter
+    {
+        [Inject] SingletoneServices Singletone { get; set; }
+        [Inject]  TransientServices Transient { get; set; }
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            Singletone.Value = currentCount;
+            Transient.Value = currentCount;
+        }
+    }
+}
