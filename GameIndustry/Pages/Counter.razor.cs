1b455e9a9de19de7010c7953cf8517bdebab3608
Jacob <akella2609@gmail.com>
2022-04-23 22:32:39 +0200

    CascadingParameters/body style switcher

--- GameIndustry/Pages/Counter.razor.cs
+++ GameIndustry/Pages/Counter.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using GameIndustry.Models;
+using Microsoft.AspNetCore.Components;
+using static GameIndustry.Shared.MainLayout;
 
 namespace GameIndustry.Pages
 
@@ -7,6 +9,9 @@ namespace GameIndustry.Pages
     {
         [Inject] SingletoneServices Singletone { get; set; }
         [Inject]  TransientServices Transient { get; set; }
+        [CascadingParameter(Name = "Style")] public AppStyles Style { get; set; }
+        //[CascadingParameter(Name ="Color")] public string Color { get; set; }
+        //[CascadingParameter(Name = "Size")] public string Size { get; set; }
         private int currentCount = 0;
 
         private void IncrementCount()
